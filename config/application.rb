7439df400c8aa5b52ff734e6fddd16e6362ae8f6
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-22 14:44:40 +0900

    タイムゾーン && 不要なファイルが作成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.load_defaults 5.2
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

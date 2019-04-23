623c3db9da9db2511e8fa165b6973aa5fb3fbcaa
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-23 18:18:34 +0900

    全機能実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,46 @@
 class PropertiesController < ApplicationController
+  before_action :set_propety,only:[:show,:edit,:update,:destroy]
   def index
+    @properties = Property.all
+  end
+
+  def new
+    @property = Property.new
+    2.times { @property.stations.build }
+  end
+
+  def create
+    if Property.create(property_params)
+      redirect_to properties_path
+    else
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to properties_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_path
   end
 
   def property_params
-    params.require(:property).permit(:name,:price,:address,:age,:remarks)
+    params.require(:property).permit(:name,:price,:address,:age,:remarks,stations_attributes:[:id,:line,:station_name,:walktime])
+  end
+
+  def set_propety
+    @property = Property.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190422081614) do
+ActiveRecord::Schema.define(version: 20190423075355) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20190422081614) do
     t.text     "remarks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "line"
+    t.string   "station_name"
+    t.integer  "walktime"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "property_id"
+    t.index ["property_id"], name: "index_stations_on_property_id"
   end
 
 end
